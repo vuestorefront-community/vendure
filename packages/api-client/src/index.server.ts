5b653af0b7e6496da060c98ed671a17d795b8f35
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-08-04 17:51:07 +0200

    feat(api): create apollo connection

--- packages/api-client/src/index.server.ts
+++ packages/api-client/src/index.server.ts
@@ -1,23 +1,36 @@
 import { apiClientFactory } from '@vue-storefront/core';
-import getProduct from './api/getProduct';
-import getCategory from './api/getCategory';
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createHttpLink } from 'apollo-link-http';
+import { ApolloLink } from 'apollo-link';
+import fetch from 'isomorphic-fetch';
+// import type { Settings, Endpoints } from './types';
+import * as api from './api';
 
 const defaultSettings = {};
 
-const onCreate = (settings) => ({
-  config: {
-    ...defaultSettings,
-    ...settings
-  },
-  client: {}
-});
+// TODO: add types
+function onCreate(settings): { config, client } {
+  const httpLink = createHttpLink({ uri: settings.api.uri, fetch });
+  return {
+    config: {
+      ...settings,
+      ...defaultSettings
+    },
+    client: new ApolloClient({
+      cache: new InMemoryCache(),
+      link: ApolloLink.from([httpLink])
+    })
+  };
+}
+
+// TODO: add extensions here later
+const extensions = [];
 
 const { createApiClient } = apiClientFactory<any, any>({
   onCreate,
-  api: {
-    getProduct,
-    getCategory
-  }
+  api,
+  extensions
 });
 
 export {
