cdec3c00a4d9e158e6cbc257aa2d4bf76f9ac74f
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-08-04 17:57:08 +0200

    feat(theme): create middleware.config

--- packages/theme/middleware.config.js
+++ packages/theme/middleware.config.js
@@ -1,8 +1,17 @@
 module.exports = {
   integrations: {
     vendure: {
-      location: "@vue-storefront/vendure-api/server",
-      configuration: {}
+      location: '@vue-storefront/vendure-api/server',
+      configuration: {
+        api: {
+          // for demo replace with https://demo.vendure.io/shop-api
+          uri: 'http://localhost:3000/shop-api',
+          // to be used later with authentication
+          tokenMethod: 'cookie'
+        },
+        currency: 'USD',
+        lang: 'en'
+      }
     }
   }
 };
