093399d5995f79e08a1615d116e30c51ff154051
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-08-11 08:21:41 +0200

    feat(docs): add VuePress docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  title: 'Vue Storefront 2 for <% INTEGRATION %>',
+  base: '/',
+  description: 'Documentation for the <% INTEGRATION %> connector for Vue Storefront 2',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.png' }]
+  ],
+  configureWebpack: (config) => {
+    config.module.rules = config.module.rules.map(rule => ({
+      ...rule,
+      use: rule.use && rule.use.map(useRule => ({
+        ...useRule,
+        options: useRule.loader === 'url-loader' ?
+          /**
+            Hack for loading images properly.
+            ref: https://github.com/vuejs/vue-loader/issues/1612#issuecomment-559366730
+           */
+          {  ...useRule.options, esModule: false } :
+          useRule.options
+      }))
+    }))
+  },
+  themeConfig: {
+    sidebar: [
+      {
+        title: 'Essentials',
+        collapsable: false,
+        children: [
+          ['/', 'Introduction'],
+          ['/guide/getting-started', 'Getting started'],
+          ['/guide/configuration', 'Configuration'],
+          ['/guide/about', 'About'],
+        ]
+      },
+    ]
+  }
+}
