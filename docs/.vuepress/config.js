b28783046813f4144097244cde3063452739d702
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-08-12 18:54:28 +0200

    feat(docs): rename integration docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  title: 'Vue Storefront 2 for <% INTEGRATION %>',
+  title: 'Vue Storefront 2 for Vendure',
   base: '/',
-  description: 'Documentation for the <% INTEGRATION %> connector for Vue Storefront 2',
+  description: 'Documentation for the Vendure connector for Vue Storefront 2',
   head: [
     ['link', { rel: 'icon', href: '/favicon.png' }]
   ],
