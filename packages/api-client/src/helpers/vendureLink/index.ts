d5c16f8ea6f74133cef8e63f27aaf1bde11c2866
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-08-12 10:30:29 +0200

    feat(api): create vendure link helpers

---
+++ packages/api-client/src/helpers/vendureLink/index.ts
@@ -0,0 +1,20 @@
+
+import { Logger } from '@vue-storefront/core';
+import { Config } from '../../types/setup';
+import { apolloLinkFactory } from './graphQL';
+import { authLinkFactory } from './linkHandlers';
+
+export const createVendureConnection = (settings: Config): any => {
+  Logger.debug('createVendureConnection');
+
+  // TODO: Configure later when token management will be needed
+//   const authLink = authLinkFactory({ state: settings.state });
+
+  const apolloLink = apolloLinkFactory(settings, 
+    // { authLink }
+    );
+
+  return {
+    apolloLink,
+  };
+};
