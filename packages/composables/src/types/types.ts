3ff8c59eb37ae2fb138c421388cb756830681ab8
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-08-12 16:37:58 +0200

    fix(api): move types to separate folder

---
+++ packages/composables/src/types/types.ts
@@ -0,0 +1,21 @@
+export type TODO = any;
+
+export type UseBillingAddParams = TODO;
+
+export type UseCategorySearchParams = TODO;
+
+export type UseFacetSearchParams = TODO;
+
+export type UseProductSearchParams = TODO;
+
+export type UseReviewSearchParams = TODO;
+
+export type UseReviewAddParams = TODO;
+
+export type UseShippingAddParams = TODO;
+
+export type UseUserUpdateParams = TODO;
+
+export type UseUserRegisterParams = TODO;
+
+export type useUserOrderSearchParams = TODO;
