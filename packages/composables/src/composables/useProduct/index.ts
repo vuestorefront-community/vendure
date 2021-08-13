2fc298dca3855ec5e43a959ff8e455b21c02c5d3
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-08-13 12:40:33 +0200

    fix: refactor useProduct to return raw response

--- packages/composables/src/composables/useProduct/index.ts
+++ packages/composables/src/composables/useProduct/index.ts
@@ -1,40 +1,15 @@
-import { useProductFactory, UseProduct, Context, CustomQuery } from '@vue-storefront/core';
+import { useProductFactory, UseProduct, Context, CustomQuery, UseProductFactoryParams } from '@vue-storefront/core';
 import type { Product, ProductFilter, ProductParams } from '@vue-storefront/vendure-api';
 
-interface SearchParams extends ProductParams {
-  customQuery?: CustomQuery;
-}
-
-const useProductFactoryParams = {
-  productsSearch: async (context: Context, { customQuery, ...searchParams }: SearchParams): Promise<Product[]> => {
-    const productResponse = await context.$vendure.api.getProduct(searchParams, customQuery);
-
-    const { variants, ...rest } = productResponse?.data?.product;
-    const productVariants = variants.map(variant => {
-      return {
-        _id: variant.id,
-        _description: rest.description,
-        _categoriesRef: rest.collections.map(collection => collection.id),
-        name: variant.name,
-        sku: variant.sku,
-        slug: rest.slug,
-        images: [rest.featuredAsset],
-        price: {
-          original: variant.price,
-          current: variant.priceWithTax
-        },
-        collections: rest.collections,
-        optionGroups: rest.optionGroups,
-        featuredAsset: rest.featuredAsset,
-        assets: rest.assets
-      };
-    });
-
-    return productVariants;
+const useProductFactoryParams: UseProductFactoryParams<Product, ProductFilter> = {
+  productsSearch: async (context: Context, params?: ProductParams & { customQuery?: CustomQuery }): Promise<Product> => {
+    const { customQuery, ...productSearchParams } = params;
+    const productResponse = await context.$vendure.api.getProduct(productSearchParams, customQuery);
+    return productResponse?.data?.product;
   }
 };
 
-const useProduct: (cacheId: string) => UseProduct<Product[], ProductFilter> = useProductFactory<Product[], ProductFilter>(useProductFactoryParams);
+const useProduct: (cacheId: string) => UseProduct<Product, ProductFilter> = useProductFactory<Product, ProductFilter>(useProductFactoryParams);
 
 export {
   useProduct,
