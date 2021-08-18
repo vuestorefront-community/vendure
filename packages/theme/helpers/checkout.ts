6b18b6503ea92a575b28a33cd1f37ddf750baad3
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-08-18 19:22:14 +0200

    feat: (theme) add components for cart flow

---
+++ packages/theme/helpers/checkout.ts
@@ -0,0 +1,7 @@
+export const getShippingMethodPrice = (shippingMethod, total: number): number => {
+  const centAmount = shippingMethod?.zoneRates[0].shippingRates[0].freeAbove?.centAmount;
+  if (centAmount && total >= (centAmount / 100)) {
+    return 0;
+  }
+  return shippingMethod.zoneRates[0].shippingRates[0].price.centAmount / 100;
+};
