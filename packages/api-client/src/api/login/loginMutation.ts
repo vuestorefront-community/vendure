179b9a92b74e364f920a0f6f2e8fd05de15a532d
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-08-27 16:39:24 +0200

    feat: #14 (api) improve query

--- packages/api-client/src/api/login/loginMutation.ts
+++ packages/api-client/src/api/login/loginMutation.ts
@@ -1,24 +1,18 @@
 import gql from 'graphql-tag';
+import { ErrorResultFragment } from '../../fragments';
 
 export default gql`
+    ${ErrorResultFragment}
+
     mutation login($username: String!, $password: String!, $rememberMe: Boolean) {
         login(username: $username, password: $password, rememberMe: $rememberMe) {
             ...on CurrentUser {
                 id
                 identifier
             }
+            ...ErrorResult
             ... on InvalidCredentialsError {
-                errorCode
-                message
                 authenticationError
-            }
-            ... on NotVerifiedError {
-                errorCode
-                message
-            }
-            ... on NativeAuthStrategyError {
-                errorCode
-                message
             }
         }
     }
