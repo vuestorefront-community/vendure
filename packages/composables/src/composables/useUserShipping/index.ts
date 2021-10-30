cceb3c59160e96a4a154468b2dd2f77c34949553
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-10-30 12:47:21 +0200

    feat: #73 create theme components

--- packages/composables/src/composables/useUserShipping/index.ts
+++ packages/composables/src/composables/useUserShipping/index.ts
@@ -3,34 +3,57 @@ import {
   useUserShippingFactory,
   UseUserShippingFactoryParams
 } from '@vue-storefront/core';
+import type { CreateAddressInput, DeleteCustomerAddress, Success, UpdateAddressInput } from '@vue-storefront/vendure-api';
+import { useUser } from '../useUser';
 import type {
-  UserShippingAddress as Address,
-  UserShippingAddressItem as AddressItem
+  UserBillingAddress as Address,
+  UserBillingAddressItem as AddressItem
 } from '@vue-storefront/vendure-api';
 
 const params: UseUserShippingFactoryParams<Address, AddressItem> = {
+  provide() {
+    return {
+      user: useUser()
+    };
+  },
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  addAddress: async (context: Context, params) => {
-    console.log('Mocked: useUserShipping.addAddress');
-    return {};
+  addAddress: async (context: Context, params): Promise<Address> => {
+    console.log('Mocked: useUserShipping.addAddress', params);
+    const shippingAddress: CreateAddressInput = {
+      ...params?.address,
+      defaultShippingAddress: (params?.address as any).isDefault
+    } as CreateAddressInput
+    
+    const result = await context.$vendure.api.createCustomerAddress(shippingAddress, params?.customQuery);
+
+    return result?.data?.createCustomerAddress;
   },
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  deleteAddress: async (context: Context, params) => {
-    console.log('Mocked: useUserShipping.deleteAddress');
-    return {};
+  deleteAddress: async (context: Context, params): Promise<Success> => {
+    console.log('Mocked: useUserShipping.deleteAddress', params);
+
+    const result = await context.$vendure.api.deleteCustomerAddress(params?.address, params?.customQuery);
+    return result?.data?.deleteCustomerAddress;
   },
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  updateAddress: async (context: Context, params) => {
-    console.log('Mocked: useUserShipping.updateAddress');
-    return {};
+  updateAddress: async (context: Context, params): Promise<Address> => {
+    console.log('Mocked: useUserShipping.updateAddress', params);
+
+    const shippingAddress: UpdateAddressInput = {
+      ...params?.address,
+      defaultShippingAddress: (params?.address as any).isDefault
+    } as UpdateAddressInput
+
+    const result = await context.$vendure.api.updateCustomerAddress(shippingAddress, params?.customQuery);
+
+    return result?.data?.updateCustomerAddress;
   },
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   load: async (context: Context, params) => {
-    console.log('Mocked: useUserShipping.load');
-    return {};
+    return context.user?.user?.value?.addresses;
   },
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
