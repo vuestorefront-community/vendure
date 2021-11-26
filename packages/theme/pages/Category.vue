b541f66ea942a977b1b217efcf1a38f9d47ce814
AdamPawlinski <apawlinski@vuestorefront.io>
2021-11-26 10:14:17 +0100

    fix: product card horizontal default value

--- packages/theme/pages/Category.vue
+++ packages/theme/pages/Category.vue
@@ -182,7 +182,7 @@
               v-e2e="'category-product-card'"
               v-for="(product, i) in products"
               :key="productGetters.getSlug(product)"
-              :qty="1"
+              :qty="itemQuantity"
               :style="{ '--index': i }"
               :title="productGetters.getName(product)"
               :description="productGetters.getDescription(product)"
@@ -194,7 +194,7 @@
               class="products__product-card-horizontal"
               @input="productQuantity[product._id] = $event"
               @click:wishlist="!isInWishlist({ product }) ? addItemToWishlist({ product }) : removeItemFromWishlist({ product })"
-              @click:add-to-cart="addItemToCart({ product, quantity: Number(productQuantity[product._id]) })"
+              @click:add-to-cart="addItemToCart({ product, quantity:  Number(productQuantity[product._id]) || itemQuantity })"
               :link="localePath(`/p/${productGetters.getId(product)}/${productGetters.getSlug(product)}`)"
             >
               <template #configuration>
@@ -352,6 +352,7 @@ export default {
   transition: 'fade',
   setup(props, context) {
     const productQuantity = ref({});
+    const itemQuantity = ref(1);
     const th = useUiHelpers();
     const uiState = useUiState();
     const { addItem: addItemToCart, isInCart, cart } = useCart();
@@ -470,7 +471,8 @@ export default {
       selectedFilters,
       clearFilters,
       applyFilters,
-      cart
+      cart,
+      itemQuantity
     };
   },
   components: {
