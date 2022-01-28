3b6fde849e959c18d1bf98c2487d314af67090c0
AdamPawlinski <apawlinski@vuestorefront.io>
2022-01-28 15:07:42 +0100

    feat: add currency extention

---
+++ packages/api-client/src/extensions/localiseConfig.ts
@@ -0,0 +1,20 @@
+import { ApiClientExtension } from '@vue-storefront/core';
+
+function getLocalisedUri(request, configuration) {
+  const originalUri = configuration.api.uri;
+  const api = { uri: originalUri + '?languageCode=' + request.cookies.i18n_redirected + '&currencyCode=' + request.cookies['vsf-currency']};
+  console.log(api);
+  return { api };
+}
+
+export const localiseExtension: ApiClientExtension = {
+  name: 'localiseExtension',
+  hooks(request) {
+    return {
+      beforeCreate: ({ configuration }) => ({
+        ...configuration,
+        ...getLocalisedUri(request, configuration)
+      })
+    };
+  }
+};
